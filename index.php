aa6ccb69a90046a5368565dcb6eb3c49e53c42f8
Hristo Hristov <koredalin@gmail.com>
2023-08-19 10:29:48 +0300

    Food Drivers project init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Food Drivers</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Food Drivers</h1>
+        <?php
+        require 'vendor/autoload.php';
+                
+        Drivers\Models\DriverBalancingSimulation::printTestStr();
+        ?>
+    </body>
+</html>
---
+++ src/Models/DriverBalancingSimulation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drivers\Models;
+
+/**
+ * Description of DriverBalancingSimulation
+ *
+ * @author H1
+ */
+class DriverBalancingSimulation {
+    public static function printTestStr()
+    {
+        echo '<h3>Test String.</h3>';
+    }
+}
